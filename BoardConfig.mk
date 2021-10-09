48c57d6e55e119f5f26091283bace69595da4f6c
Roger Ellis <ellisr@google.com>
2021-10-09 15:30:23 +0200

    [DNM] surya: Add exclusion for vendor ELF prebuilts
     - ELF prebuilts were banned in PRODUCT_COPY_FILES starting May 11.
     - Adding BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true to
    BoardConfig.mk temoporarily.
     - See b/156534160 for long-term fix which should override this change.
    
    Test: Build
    Bug: 156534160
    Change-Id: I7d80798d3b2d3818bc835fcede21de2e7ab04735

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ BOARD_VENDOR := xiaomi
 DEVICE_PATH := device/xiaomi/surya
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Inherit from proprietary files
 include vendor/xiaomi/surya/BoardConfigVendor.mk
